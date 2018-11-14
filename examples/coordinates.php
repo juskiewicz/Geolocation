d3934c0ca51409982fb9721f2ff07b3aabbcd4f3
Tomasz Juśkiewicz <to.juskiewicz@gmail.com>
2018-11-14 20:27:16 +0100

    Aktualizacja przykładu

--- examples/coordinates.php
+++ examples/coordinates.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Przykład pobrania lokalizacji przy użyciu obiektu addresss
+ * Przykłady pobrania lokalizacji
  */
 
 require __DIR__.'/../vendor/autoload.php';
@@ -8,7 +8,9 @@ require __DIR__.'/../vendor/autoload.php';
 use Juskiewicz\Geolocation\Geolocation;
 use Juskiewicz\Geolocation\Model\Address;
 
-// create address object
+/*
+ * Pobranie lokalizacji za pomocą obiektu
+ */
 $address = new Address(
     null,
     'Maltańska',
@@ -19,6 +21,13 @@ $address = new Address(
 );
 
 $geolocation = new Geolocation('YOUR_GOOGLE_MAPS_API_KEY', null, null);
-$coordinates = $geolocation->getCoordinates($address);
+$coordinates = $geolocation->getCoordinatesByObject($address);
+var_dump($coordinates);
 
+/*
+ * Pobranie lokalizacji za pomocą stringu
+ */
+$address = "Maltańska 1, 61-131 Poznań, Polska";
+$geolocation = new Geolocation('YOUR_GOOGLE_MAPS_API_KEY', null, null);
+$coordinates = $geolocation->getCoordinatesByString($address);
 var_dump($coordinates);
